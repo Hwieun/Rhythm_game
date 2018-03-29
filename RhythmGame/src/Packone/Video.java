23716f5e91e34478e0f1e82826566355af9d7b89
Hwieun <gk1vk@naver.com>
2018-03-29 18:52:18 +0900

    fix HSV range

--- RhythmGame/src/Packone/Video.java
+++ RhythmGame/src/Packone/Video.java
@@ -53,7 +53,7 @@ public class Video extends Thread {
 	}
 
 	public Image readCam() {
-if (camera.read(origin)) { // 읽어서 Mat타입으로 저장
+		if (camera.read(origin)) { // 읽어서 Mat타입으로 저장
 			handDetect();
 			ImageIcon cImg = new ImageIcon(Mat2BufferedImage(origin));
 			return cImg.getImage();
@@ -63,31 +63,40 @@ if (camera.read(origin)) { // 읽어서 Mat타입으로 저장
 
 	public void handDetect() {
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-		// List<MatOfPoint> hull = new ArrayList<Mat>(contours.size());
 		Mat hierarchy = new Mat();
 		int threshold1 = 85;
-		Scalar skincolorLower = new Scalar(1, 50, 32);
-		Scalar skincolorUpper = new Scalar(170, 150, 0.69 * 255);
+		Scalar skincolorLower = new Scalar(0, 0.23*255, 32);
+		Scalar skincolorUpper = new Scalar(50, 0.68*255, 255);
 		Imgproc.resize(origin, origin, sz);
 		Core.flip(origin, origin, 1); // y축 기준으로 뒤집기
 		Imgproc.cvtColor(origin, hand, Imgproc.COLOR_BGR2HSV);
 		Core.inRange(hand, skincolorLower, skincolorUpper, hand);
-		Imgproc.blur(hand, hand, new Size(5, 5));
-		Imgproc.threshold(hand, hand, 200, 255, Imgproc.THRESH_BINARY);
-		Mat verticalStructure = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(1, hand.rows()/50));
+//		Imgproc.blur(hand, hand, new Size(5, 5));
+//		Imgproc.threshold(hand, hand, 200, 255, Imgproc.THRESH_BINARY);
+		Mat verticalStructure = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(1, hand.rows() / 50));
 		Imgproc.erode(hand, hand, verticalStructure);
 		Imgproc.erode(hand, hand, verticalStructure);
 		Imgproc.dilate(hand, hand, verticalStructure);
 		Imgproc.dilate(hand, hand, verticalStructure);
 		Imgproc.findContours(hand, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-		for (int i = 1; i < contours.size(); i++)
-			Imgproc.drawContours(origin, contours, i, new Scalar(0, 0, 255), 2, 8, hierarchy, 0, new Point());
-
-//		Imgproc.connectedComponents(hand, labels);
-
+		Point centerOfHand = new Point();
+		for (int i = 0; i < contours.size(); i++)
+		{	
+			Imgproc.drawContours(origin, contours, i, new Scalar(0, 0, 255),3);
+			
+		}
+		//
+		contours.clear();
+/*		List<MatOfInt> hull = new ArrayList<MatOfInt>(contours.size());
+		List<MatOfInt4> defects = new ArrayList<MatOfInt4>(contours.size());
+		for (int i = 0; i < contours.size(); i++) {
+			Imgproc.convexHull(contours.get(i), hull.get(i));
+//			Imgproc.convexityDefects(contours.get(i), hull.get(i), defects.get(i));
+			Imgproc.fillConvexPoly(origin, contours.get(i), new Scalar(0,255,255));
+		}*/
 		
 	}
-
+	
 	public void close() {
 		camera.release();
 		this.interrupt();
