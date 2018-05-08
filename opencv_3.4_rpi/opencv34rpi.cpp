8cee4f80e6eb3a8369b28f5ed2a98984d0ba09b3
Hwieun <gk1vk@naver.com>
2018-05-08 21:55:03 +0900

    convexity defects

--- opencv_3.4_rpi/opencv34rpi.cpp
+++ opencv_3.4_rpi/opencv34rpi.cpp
@@ -1,48 +1,161 @@
 #include "stdafx.h"
-#include <opencv2\opencv.hpp>
+//#include <opencv2\opencv.hpp>
 //#include <opencv2\imgproc.hpp>
-#include <opencv2\highgui.hpp>
+//#include <opencv2\highgui.hpp>
 #include <opencv2\core\core.hpp>
 #include <iostream>
+#include <time.h>
+#include <math.h>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
 
-#define PI 3.1415926
+
 
 using namespace cv;
+using namespace std;
 
-int main() {
-	VideoCapture video("C:\\Users\\Kim Hwieun\\Desktop\\kkk\\test.mp4");
-	Mat img, bfimg;
-//	Mat contours;
-	if (!video.isOpened()) return 0;
-
-	int thres = 85;
-	std::vector<Vec4i> lines;
-
-//	img = imread("C:\\Users\\Kim Hwieun\\Desktop\\kkk\\cap.PNG", IMREAD_COLOR);
-	Mat houghImg = img.clone();
-
-	cvtColor(img, img, COLOR_BGR2GRAY);		
-	GaussianBlur(img, img, Size(3, 3), 0, 0, BORDER_DEFAULT);
-	Canny(img, img, thres, thres*2, 3);
-//	Sobel(img, bfimg, CV_16S, 1, 0, 3, 1, 0, BORDER_DEFAULT);
-//	convertScaleAbs(bfimg, img);
-	HoughLinesP(img, lines, 1, CV_PI / 120, 50,0,0);
-
-	thres = 1;
-	lines.clear();
-	HoughLinesP(img, lines, 1, CV_PI / 180, 70, 0, 0);
-	for (size_t i = 0; i < lines.size(); i++) {
-		Vec4i l = lines[i];
-		if (l[1] / thres != l[3] / thres) //가로 줄 걸러냄
-			line(houghImg, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 1, CV_AA);
-//			rectangle(houghImg, rec, Scalar(0, 0, 255));
+class Video {
+private:
+	VideoCapture camera;
+	Mat origin, hand, labels, imgROI, drawROI;
+	Size sz;
+	float fps;
+	float curTime;
+public:
+	Video() {
+		sz.width = 1280;
+		sz.height = 720;
+		camera = VideoCapture(0);
+		if (!camera.isOpened())
+			printf("Camera not connected!");
 	}
 
-	namedWindow("Hough", CV_WINDOW_AUTOSIZE);
-	imshow("Hough", houghImg);
+	void readCam() {
+		char ch = 0;
+		while (camera.read(origin))
+		{
+			if (ch == 27) close();
+			curTime = clock();
+			handDetect();
+			curTime = (float)(clock() - curTime);
+//			printf("curTime : %f\n", curTime);
+			fps = (1 / curTime)*1000;
+			printf("fps : %f\n", fps);
+			ch = waitKey(1); //waitKey의 delay 단위는 ms
+			imshow("Hough", origin);
+		}
+	}
 
-//	video.release();	
-	waitKey(0);
-	cv::destroyAllWindows();
+	void handDetect() {
+		vector<vector<Point>> contours;
+		Mat hierarchy;
+		Rect ROI(400, 220, 450, 400);
+
+		resize(origin, origin, sz);
+		flip(origin, origin, 1); // y축 기준으로 뒤집기
+		cvtColor(origin, hand, COLOR_RGB2YCrCb);
+		inRange(hand, Scalar(0, 77, 130), Scalar(255, 133, 180), hand);
+		Mat verticalStructure = getStructuringElement(MORPH_RECT, Size(1, hand.rows / 50));
+		erode(hand, hand, verticalStructure);
+		erode(hand, hand, verticalStructure);
+		dilate(hand, hand, verticalStructure);
+		dilate(hand, hand, verticalStructure);
+		imgROI = Mat(hand, ROI);
+		drawROI = Mat(origin, ROI);
+		findContours(imgROI, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE, Point(0,0));
+		int bigsize = 0;
+		int index = 0;
+
+		rectangle(origin, Point(400, 220), Point(850, 620), Scalar(255, 255, 0), 3); //draw ROI
+
+		//calculate biggest contour
+		if (contours.size() > 0) {
+			for (int i = 0; i < contours.size(); i++)
+			{
+				double a = contourArea(contours[i], false);
+				if (a > bigsize) {
+					bigsize = a;
+					index = i;
+				}
+			}
+			vector<vector<Vec4i>> convexity(contours.size());
+			vector<vector<int>> hullint(contours.size());
+			vector<vector<Point>> hull(contours.size());
+			for (int i = 0; i < contours.size(); i++) {
+				convexHull(contours[i], hull[i], false); //false면 index, true면 point
+				convexHull(contours[i], hullint[i], false); 
+				if (hullint[i].size() > 3)
+					convexityDefects(contours[i], hullint[i], convexity[i]);
+			}
+		
+			int j = 0;
+			double x = 0, y = 0;
+			//int distance_thre = 200;
+			if (index >= 0) {
+				vector<Point> fingertips;
+				// calculate center point of biggest contour
+				for (int i = 0; i < hull.size(); i++) {
+					for (j = 0; j < hull[i].size(); j++) {
+						x = x + hull[i][j].x;
+						y = y + hull[i][j].y;
+					}
+				}
+				--j;
+				int bf = hull.size()*j;
+				printf("%d\n", hull.size());
+				Point centerOfHand = Point(x / bf, y / bf);
+				
+				/*for (int i = 0; i < hull.size(); i++) {
+					int max = 0, maxindex = 0;
+					for (j = 0; j < hull[i].size(); j++) {
+						int bf = (int)sqrt(pow(centerOfHand.x - hull[i][j].x, 2) + pow(centerOfHand.y - hull[i][j].y, 2));
+						if (max < bf) { max = bf; maxindex = j; }
+					}
+					fingertips.push_back(hull[i][maxindex]);
+				}*/
+
+				// Draw on origin	
+//				drawContours(origin, contours, index, Scalar(0, 0, 255), 3); //skin color contours
+				for (int i = 0; i < hull.size(); i++)
+					drawContours(drawROI, hull, i, Scalar(0, 255, 255), 3); //convex hull
+				for (int i = 0; i < contours.size(); i++)
+				{
+					for (const Vec4i& v : convexity[i]) {
+						float depth = v[3] / 256;
+						if (depth > 15) {
+							int startidx = v[0]; Point ptstart(contours[i][startidx]);
+							int endidx = v[1]; Point ptend(contours[i][endidx]);
+							int faridx = v[2]; Point ptfar(contours[i][faridx]);
+
+					//		line(drawROI, ptstart, ptend, Scalar(255, 0, 255), 1);
+					//		line(origin, ptstart, ptfar, Scalar(255, 0, 255), 1);
+					//		line(origin, ptfar, ptend, Scalar(255, 0, 255), 1);
+					//		circle(origin, ptfar, 3, Scalar(0, 0, 255), 2);
+							fingertips.push_back(ptstart);
+							fingertips.push_back(ptend);
+						}
+					}
+				}
+				if (centerOfHand.x > 0 && centerOfHand.y > 0 && centerOfHand.x < 250 && centerOfHand.y < 300)
+					circle(drawROI, centerOfHand, 10, Scalar(255, 255, 0), 3);
+				for (int i = 0; i < fingertips.size(); i++)
+					circle(drawROI, fingertips[i], 10, Scalar(0, 0, 255), 3);
+			}
+		}
+	}
+
+	void close() {
+		camera.release();
+		cv::destroyAllWindows();
+	}
+};
+
+int main() {
+	Video video;
+	int thres = 95, num = 0;
+	namedWindow("Hough", CV_WINDOW_AUTOSIZE);
+
+	video.readCam();
+
 	return 0;
 }
